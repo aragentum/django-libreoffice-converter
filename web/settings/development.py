f5309c2dc3618d5824b4d47b931e7f19ea664ce9
Arag Entum <aragentum@gmail.com>
2019-12-01 12:28:37 +0300

    init project

---
+++ web/settings/development.py
@@ -0,0 +1,29 @@
+from .common import *
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
+    }
+}
