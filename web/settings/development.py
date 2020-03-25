f36e7381c51157d2758757e172d29462c5841520
Arag Entum <aragentum@gmail.com>
2020-03-25 21:55:11 +0300

    expanded logging configuration

--- web/settings/development.py
+++ web/settings/development.py
@@ -9,22 +9,38 @@ ALLOWED_HOSTS = ['*']
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'console_formatter': {
+            'format': '[%(asctime)s]:[%(process)d] | [%(levelname)s] | %(name)-15s - %(message)s'
         }
     },
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'formatter': 'console_formatter'
         },
     },
     'loggers': {
         '': {
             'handlers': ['console'],
-            'level': 'DEBUG'
+            'level': 'INFO'
         },
+        'web.api': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        }
     }
 }
--- web/settings/production.py
+++ web/settings/production.py
@@ -8,16 +8,22 @@ ALLOWED_HOSTS = ['*']
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
+    'formatters': {
+        'console_formatter': {
+            'format': '[%(asctime)s]:[%(process)d] | [%(levelname)s] | %(name)-15s - %(message)s'
+        }
+    },
     'handlers': {
         'console': {
-            'level': 'INFO',
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'formatter': 'console_formatter'
         },
     },
     'loggers': {
@@ -25,5 +31,15 @@ LOGGING = {
             'handlers': ['console'],
             'level': 'INFO'
         },
+        'web.api': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False
+        }
     }
 }
